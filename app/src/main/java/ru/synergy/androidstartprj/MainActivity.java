665a7b08a48609e1e23cc8c273467e97c5adb56e
«Elena <helenvit599@gmail.com>
2023-02-13 21:45:28 +0500

    Added Java Screen configuration examples with Constrain Layout branch xmlconfig

--- app/src/main/java/ru/synergy/androidstartprj/MainActivity.java
+++ app/src/main/java/ru/synergy/androidstartprj/MainActivity.java
@@ -1,6 +1,7 @@
 package ru.synergy.androidstartprj;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.widget.TextView;
@@ -10,14 +11,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.second_layout);
 
-        TextView textView = new TextView(this);
 
-        textView.setText("Hello Android!");
+        TextView textView =(TextView) findViewById(R.id.header);
+        textView.setText("Hello from Java");
 
-        textView.setTextSize(22);
-
-        setContentView(textView);
+//        ConstraintLayout constraintLayout = new ConstraintLayout(this);
+//        TextView textView = new TextView(this);
+//        textView.setText("Hello Android!");
+//        textView.setTextSize(28);
+//
+//        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
+//                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+//                ConstraintLayout.LayoutParams.WRAP_CONTENT);
+//
+//        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
+//        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+//        layoutParams.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;
+//
+//        textView.setLayoutParams(layoutParams);
+//
+//        constraintLayout.addView(textView);
+//
+//        setContentView(constraintLayout);
     }
 }
