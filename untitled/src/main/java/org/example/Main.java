8fb59aa582f8cdb97997c6e5ea70325f4f3ea577
«Elena <helenvit599@gmail.com>
2023-02-13 20:21:24 +0500

    Added Java Screen configuration examples

---
+++ untitled/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        try(FileWriter writer = new FileWriter("notes3.txt", false))
+        {
+            // запись всей строки
+            String text = "Hello Gold!";
+            writer.write(text);
+            // запись по символам
+            writer.append('\n');
+            writer.append('E');
+
+            writer.flush();
+        }
+        catch(IOException ex) {
+
+            System.out.println(ex.getMessage());
+        }
+    }
+}
