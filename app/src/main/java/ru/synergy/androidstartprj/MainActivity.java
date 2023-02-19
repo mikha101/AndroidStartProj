7bfd61fa4ad896c29976bb07cbda0aab5c66865d
«Elena <helenvit599@gmail.com>
2023-02-19 19:11:45 +0500

    Clean Up of project

--- app/src/main/java/ru/synergy/androidstartprj/MainActivity.java
+++ app/src/main/java/ru/synergy/androidstartprj/MainActivity.java
@@ -13,34 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.second_layout);
 
-        TextView textView = (TextView)findViewById(R.id.normal);
-
-        textView.setText("Text from Java code");
-        textView.setTextColor (Color.RED);
-
-
-//        TextView textView =(TextView) findViewById(R.id.header);
-//        textView.setText("Hello from Java");
-
-//        ConstraintLayout constraintLayout = new ConstraintLayout(this);
-//        TextView textView = new TextView(this);
-//        textView.setText("Hello Android!");
-//        textView.setTextSize(28);
-//
-//        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
-//                ConstraintLayout.LayoutParams.WRAP_CONTENT,
-//                ConstraintLayout.LayoutParams.WRAP_CONTENT);
-//
-//        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
-//        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
-//        layoutParams.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;
-//
-//        textView.setLayoutParams(layoutParams);
-//
-//        constraintLayout.addView(textView);
-//
-//        setContentView(constraintLayout);
     }
 }
