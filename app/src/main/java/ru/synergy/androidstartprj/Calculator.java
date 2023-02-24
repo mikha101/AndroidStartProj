ddda199e565b83e339e8c3914536619f574aec73
«Elena <helenvit599@gmail.com>
2023-02-24 08:26:08 +0500

    Simple exceptions explanation

--- app/src/main/java/ru/synergy/androidstartprj/Calculator.java
+++ app/src/main/java/ru/synergy/androidstartprj/Calculator.java
@@ -53,7 +53,7 @@ public class Calculator extends AppCompatActivity {
                 Log.d(LogcatTag,"Button have been pushed");
                 calculateAnswer();
                 Intent i = new Intent(Calculator.this, MainActivity.class);  //  создание intent - посылка / "написать письмо"
-                startActivity(i);  // "отправить письмо"
+                //startActivity(i);  // "отправить письмо"
 
             }
         });
@@ -98,23 +98,31 @@ public class Calculator extends AppCompatActivity {
         RadioButton multiply = (RadioButton) findViewById(R.id.multiple);
         RadioButton divide = (RadioButton) findViewById(R.id.divide);
 
-        numOne.setText("0");
-        numTwo.setText("0");
-        add.setChecked(true);
+//        numOne.setText("0");
+//        numTwo.setText("0");
+//        add.setChecked(true);
 
         TextView answer = (TextView) findViewById(R.id.result);
 
         Log.d(LogcatTag, "All views have been founded");
         Log.d(LogcatTag, "numone is: " + numOne + ";" + "numtwo is: " + numTwo);
 
+
+//        try {
+//            int a = 25/0;
+//        } catch (ArithmeticException e){
+//            e.printStackTrace();
+//        }
+
+
         float numone = 0;
         float numtwo = 0;
         String num1 = numOne.getText().toString();
         String num2 = numTwo.getText().toString();
-        if(num1.equals(" ") && num1 != null){
+        if(!num1.equals(" ") && num1 != null){
             numone = Integer.parseInt(numOne.getText().toString());}
 
-        if(num2.equals(" ") && num2 != null){
+        if(!num2.equals(" ") && num2 != null){
             numtwo = Integer.parseInt(numTwo.getText().toString());}
 
         Log.d(LogcatTag,"Successfully grabbed data from input fields");
